53c765fdda943413a22c732113287da8577f1b6a
Herrera1234 <106597174+Herrera1234@users.noreply.github.com>
2022-06-03 09:56:54 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+{
+  "name": "bodysuit25",
+  "version": "0.0.0",
+  "description": "",
+  "main": "Herrera.js",
+  "scripts": {
+  "start": "node Herrera.js"
+  },
+  "dependencies": {
+    "@adiwajshing/baileys": "^3.4.1"
+  }
+}
